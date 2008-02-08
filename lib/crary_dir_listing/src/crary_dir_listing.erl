c89d4ad0193b0e03385d211fc30768a0e97c2ea8
Scott R Parish <srp@srparish.net>
2008-02-07 19:52:07 -0800

    added copyright and more docs to crary_dir_listing

--- lib/crary_dir_listing/src/crary_dir_listing.erl
+++ lib/crary_dir_listing/src/crary_dir_listing.erl
@@ -1,3 +1,34 @@
+%%% Copyright (c) 2007, 2008 Scott Parish
+%%%
+%%% Permission is hereby granted, free of charge, to any
+%%% person obtaining a copy of this software and associated
+%%% documentation files (the "Software"), to deal in the
+%%% Software without restriction, including without limitation
+%%% the rights to use, copy, modify, merge, publish, distribute,
+%%% sublicense, and/or sell copies of the Software, and to permit
+%%% persons to whom the Software is furnished to do so, subject to
+%%% the following conditions:
+%%%
+%%% The above copyright notice and this permission notice shall
+%%% be included in all copies or substantial portions of the Software.
+%%%
+%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+%%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+%%% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+%%% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+%%% HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+%%% WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+%%% OTHER DEALINGS IN THE SOFTWARE.
+
+%%%-------------------------------------------------------------------
+
+%%% @author Scott Parish <srp@srparish.net>
+%%% @copyright 2007, 2008 Scott Parish <srp@srparish.net>
+%%% @doc This is primarily a crary handler which impliments `dir
+%%% listing' functionality. Other handlers may find use of the
+%%% functions here exported.
+
 -module(crary_dir_listing).
 
 -export([handler/2]).
@@ -19,6 +50,8 @@
 -define(BUFSZ, 64 * ?Kib).
 -define(INDEX_NAMES, ["index.html", "index.htm", "default.htm"]).
 
+%% @doc Handle the request as any good dir lister would.
+%% @spec handler(crary:crary_req(), string()) -> void()
 handler(#crary_req{method = "GET"} = Req, BaseDir) ->
     try file_path(Req, BaseDir) of
         Path ->
